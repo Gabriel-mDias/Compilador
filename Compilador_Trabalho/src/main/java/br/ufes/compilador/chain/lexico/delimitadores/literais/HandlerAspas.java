2859a7a9a90f88e9696ff5c63ae93f88fb8c9c2c
matheus <matheusdasilvarocha@gmail.com>
2021-03-20 17:54:06 -0300

    Adaptacao dos handles de delimitadores

--- Compilador_Trabalho/src/main/java/br/ufes/compilador/chain/lexico/delimitadores/literais/HandlerAspas.java
+++ Compilador_Trabalho/src/main/java/br/ufes/compilador/chain/lexico/delimitadores/literais/HandlerAspas.java
@@ -1,30 +1,31 @@
 /*
- * To change this template, choose Tools | Templates
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package br.ufes.compilador.chain.lexico.delimitadores.literais;
+package br.ufes.compilador.chain.lexico.tipo;
 
-import br.ufes.compilador.chain.lexico.identificadores.HandlerIdentificador;
-import br.ufes.compilador.chain.lexico.AbstractHandlerToken;
-import br.ufes.compilador.model.Categoria;
-import br.ufes.compilador.model.Token;
+import br.ufes.compilador.chain.AbstractHandler;
+import br.ufes.compilador.models.Token;
 
 /**
  *
  * @author Matheus
  */
-public class HandlerAspas extends AbstractHandlerToken {
+public class HandlerFechaChave  extends AbstractHandler{
+
+    public HandlerFechaChave(Token token) {
+        super(token);
+    }
 
     @Override
-    public void doHandle(Token pToken) {
-
+    public void executar(Token token) {
         if ((pToken.getSimbolo().toString().toLowerCase().compareTo("\"") == 0) ||
                 (pToken.getSimbolo().toString().toLowerCase().compareTo("\'") == 0)){
-            pToken.setCategoria(new Categoria("delimitador_literal_aspas"));
-        } else if (this.sucessor == null) {
-            this.setSucessor(new HandlerIdentificador());
-            this.sucessor.handle(pToken);
+            token.setCategoria("delimitador_literal_aspas");
+        } else {
+            this.setProximo(null);
         }
-
     }
+    
 }
